0b963111badaf681878970b604ebff74d08f7c84
mahsa.mokhtare <mahs.mokhtare@gmail.com>
2025-12-07 12:23:06 +0100

    translator with process.argv

---
+++ script.js
@@ -0,0 +1,62 @@
+const consonants = [
+  "b",
+  "c",
+  "d",
+  "f",
+  "g",
+  "h",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+const userInput = process.argv[2]?.toLowerCase();
+
+console.log("**********English To Piglatin Translator*********");
+console.log("Please Enter your phrase:");
+
+const array = [];
+
+for (const char of userInput) {
+  array.push(char);
+}
+let translatedPhrase = "";
+if (
+  !array.length == 0 &&
+  consonants.includes(array[0]) &&
+  vowels.includes(array[1])
+) {
+  array.push(array[0]);
+  array.push("ay");
+  translatedPhrase = array.slice(1).join("");
+  console.log(`Result: ${translatedPhrase}`);
+} else if (
+  !array.length == 0 &&
+  consonants.includes(array[0]) &&
+  consonants.includes(array[1])
+) {
+  array.push(array[0]);
+  array.push(array[1]);
+  array.push("ay");
+  translatedPhrase = array.slice(0, 2).join("");
+  console.log(`Result: ${translatedPhrase}`);
+} else if (!array.length == 0 && vowels.includes(array[0])) {
+  array.push("way");
+  translatedPhrase = array.join("");
+  console.log(`Result: ${translatedPhrase}`);
+} else {
+  console.log(`Result: ${userInput}`);
+}
