15693ff0cc487040a2f6c90fca6adb85abe81574
mahsa.mokhtare <mahs.mokhtare@gmail.com>
2025-12-07 12:37:56 +0100

    added readline module

--- script.js
+++ script.js
@@ -23,40 +23,47 @@ const consonants = [
 ];
 const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
-const userInput = process.argv[2]?.toLowerCase();
+// const userInput = process.argv[2]?.toLowerCase();
+const readline = require("readline");
 
-console.log("**********English To Piglatin Translator*********");
-console.log("Please Enter your phrase:");
-
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 const array = [];
+console.log("**********English To Piglatin Translator*********");
 
-for (const char of userInput) {
-  array.push(char);
-}
-let translatedPhrase = "";
-if (
-  !array.length == 0 &&
-  consonants.includes(array[0]) &&
-  vowels.includes(array[1])
-) {
-  array.push(array[0]);
-  array.push("ay");
-  translatedPhrase = array.slice(1).join("");
-  console.log(`Result: ${translatedPhrase}`);
-} else if (
-  !array.length == 0 &&
-  consonants.includes(array[0]) &&
-  consonants.includes(array[1])
-) {
-  array.push(array[0]);
-  array.push(array[1]);
-  array.push("ay");
-  translatedPhrase = array.slice(0, 2).join("");
-  console.log(`Result: ${translatedPhrase}`);
-} else if (!array.length == 0 && vowels.includes(array[0])) {
-  array.push("way");
-  translatedPhrase = array.join("");
-  console.log(`Result: ${translatedPhrase}`);
-} else {
-  console.log(`Result: ${userInput}`);
-}
+rl.question("Please enter your phrase: ", (input) => {
+  const userInput = input.toLocaleLowerCase();
+  for (const char of userInput) {
+    array.push(char);
+  }
+  let translatedPhrase = "";
+  if (
+    !array.length == 0 &&
+    consonants.includes(array[0]) &&
+    vowels.includes(array[1])
+  ) {
+    array.push(array[0]);
+    array.push("ay");
+    translatedPhrase = array.slice(1).join("");
+    console.log(`Result: ${translatedPhrase}`);
+  } else if (
+    !array.length == 0 &&
+    consonants.includes(array[0]) &&
+    consonants.includes(array[1])
+  ) {
+    array.push(array[0]);
+    array.push(array[1]);
+    array.push("ay");
+    translatedPhrase = array.slice(0, 2).join("");
+    console.log(`Result: ${translatedPhrase}`);
+  } else if (!array.length == 0 && vowels.includes(array[0])) {
+    array.push("way");
+    translatedPhrase = array.join("");
+    console.log(`Result: ${translatedPhrase}`);
+  } else {
+    console.log(`Result: ${userInput}`);
+  }
+  rl.close();
+});
